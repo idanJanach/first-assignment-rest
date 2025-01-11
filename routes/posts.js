b4cb996f5bf0c0e21a672cf2c78eae908e86bcf4
idan janach <idanjanach4455@gmail.com>
2025-01-11 19:12:53 +0200

    added model route and controller for post

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,22 @@
-const express = require('express')
-const Post = require('../controllers/posts')
-const router = express.Router()
+const express = require('express');
+const {
+  getAllPosts,
+  getPostById,
+  getPostsBySender,
+  addPost,
+  updatePost,
+} = require('../controllers/posts');
 
-router.get('/', Post.getAllPosts)
+const router = express.Router();
 
-module.exports = router
+router.get('/', getAllPosts);
+
+router.get('/:id', getPostById);
+
+router.get('/', getPostsBySender);
+
+router.post('/', addPost);
+
+router.put('/:id', updatePost);
+
+module.exports = router;
